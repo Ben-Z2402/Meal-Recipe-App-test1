c8751bb521d0fb4c940793b768058899dc4eba25
Yakkuu <jack2016zhang@gmail.com>
2023-10-30 18:55:12 -0400

    worked on input and output boundary for use case  4

---
+++ src/Exercise/ExerciseInputBoundary.java
@@ -0,0 +1,5 @@
+package Exercise;
+
+public interface ExerciseInputBoundary {
+    void execute(ExerciseInputData exerciseInputData);
+}
---
+++ src/Exercise/ExerciseInputData.java
@@ -0,0 +1,12 @@
+package Exercise;
+
+public class ExerciseInputData {
+    /* using Calculator.net
+    WalkingTime
+    RunningTime
+    CyclingTime
+    SwimmingTime
+    Weight
+    Intensity for each
+    */
+}
