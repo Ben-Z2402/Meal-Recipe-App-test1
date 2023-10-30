b39fe8486b476fd77ed8fb66a1b42424561c7085
Iris <irisss1717@gmail.com>
2023-10-30 17:53:19 -0400

    changed Meal to an interface

--- src/entity/UserProfileFactory.java
+++ src/entity/UserProfileFactory.java
@@ -1,4 +1,8 @@
 package entity;
 
+import java.util.ArrayList;
+
 public interface UserProfileFactory {
+    UserProfile create(float weight, float height, int age, ArrayList<String> dietaryRestrictions, float weeklyBudget,
+                       int recommendedDailyCalories);
 }
