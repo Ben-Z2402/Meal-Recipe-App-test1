0f231bb07b812a245b3d7d0491f6eec4e0c6ec2c
Iris <irisss1717@gmail.com>
2023-11-25 15:12:08 -0500

    implemented the controller for weekly diet

--- src/interface_adapter/WeeklyDietController.java
+++ src/interface_adapter/WeeklyDietController.java
@@ -1,4 +1,19 @@
 package interface_adapter;
 
+import entity.UserProfile;
+import use_case.weekly_diet.WeeklyDietInputBoundary;
+import use_case.weekly_diet.WeeklyDietInputData;
+
 public class WeeklyDietController {
+
+    final WeeklyDietInputBoundary weeklyDietUseCaseInteractor;
+    public WeeklyDietController(WeeklyDietInputBoundary weeklyDietUseCaseInteractor) {
+        this.weeklyDietUseCaseInteractor = weeklyDietUseCaseInteractor;
+    }
+
+    public void execute(UserProfile user) {
+        WeeklyDietInputData weeklyDietInputData = new WeeklyDietInputData(user);
+
+        weeklyDietUseCaseInteractor.execute(weeklyDietInputData);
+    }
 }
