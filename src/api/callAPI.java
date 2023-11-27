12c509cdbc5f04683b6fc3727dcce1d23f11839d
Ben Zheng <benz.zheng@mail.utoronto.ca>
2023-11-27 18:50:46 -0500

    modified api call

--- src/api/callAPI.java
+++ src/api/callAPI.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class CallAPI {
     private static final String API_URL = "https://api.edamam.com/api/recipes/v2?type=public&app_id=981e8b83&" +
-            "app_key=%202fba7f42e263a88f352970997e1158c3&calories=100-1000";
+            "app_key=%202fba7f42e263a88f352970997e1158c3";
 
     public static String getApiUrl() {
         return API_URL;
@@ -24,13 +24,17 @@ public class CallAPI {
         }
     }
 
-    public static JSONObject RecipeUrl() throws JSONException, IOException {
+
+    public static JSONObject RecipeUrl(String params) throws JSONException, IOException {
+
+        String fin = API_URL + params;
+
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("text/plain");
         RequestBody body = RequestBody.create(mediaType, "");
         Request request = new Request.Builder()
-                .url("https://api.edamam.com/api/recipes/v2?type=public&app_id=981e8b83&app_key=2fba7f42e263a88f352970997e1158c3")
+                .url(fin)
                 .method("GET", body)
                 .build();
         Response response = client.newCall(request).execute();
