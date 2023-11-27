e7b656a044ac76ba027a6989ee2105858da7c255
Iris <irisss1717@gmail.com>
2023-11-27 18:26:32 -0500

    started recipes view

--- src/interface_adapter/RecipesState.java
+++ src/interface_adapter/RecipesState.java
@@ -1,11 +1,50 @@
 package interface_adapter;
 
 public class RecipesState {
-    ...
+    private String recipeName = "";
+    private String description = "";
+    private String nutrients = "";
+    private String ingredients = "";
+    private String instructions = "";
 
     public RecipesState(RecipesState copy) {
-        ...
+        recipeName = copy.recipeName;
+        description = copy.description;
+        nutrients = copy.nutrients;
+        instructions = copy.instructions;
     }
 
     public RecipesState() {}
+
+    public String getRecipeName() {
+        return recipeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getNutrients() {
+        return nutrients;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setRecipeName(String recipeName) {
+        this.recipeName = recipeName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setNutrients(String nutrients) {
+        this.nutrients = nutrients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
 }
--- src/view/RecipesView.java
+++ src/view/RecipesView.java
@@ -1,4 +1,95 @@
 package view;
 
-public class RecipesView {
+import entity.UserProfile;
+import entity.UserProfileFactory;
+import interface_adapter.*;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+
+public class RecipesView extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "recipes";
+
+    private final RecipesViewModel recipesViewModel;
+    private final JLabel recipeName;
+    private final JLabel description;
+    private final JLabel nutrients;
+    private final JLabel ingredients;
+    private final JLabel instructions;
+    private final WeeklyDietController weeklyDietController;
+
+    private final JButton next;
+    private final JButton ok;
+
+    public RecipesView(WeeklyDietController controller, RecipesViewModel recipesViewModel) {
+
+        this.weeklyDietController = controller;
+        this.recipesViewModel = recipesViewModel;
+        recipesViewModel.addPropertyChangeListener(this);
+
+        JLabel title = new JLabel(recipesViewModel.TITLE_LABEL);
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JPanel recipeInfo = new JPanel();
+
+        JPanel buttons = new JPanel();
+        next = new JButton(recipesViewModel.NEXT_BUTTON_LABEL);
+        buttons.add(next);
+        ok = new JButton(recipesViewModel.OK_BUTTON_LABEL);
+        buttons.add(ok);
+    }
+
+    next.addActionListener(
+        new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                if (evt.getSource().equals(next)) {
+                    RecipesState currentState = recipesViewModel.getState();
+                    ...
+
+                    weeklyDietController.execute(...);
+                }
+            }
+        }
+    );
+
+    ok.addActionListener(
+        new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                if (evt.getSource().equals(ok)) {
+                    RecipesState currentState = recipesViewModel.getState();
+                    ...
+
+                    weeklyDietController.execute(...);
+                }
+            }
+        }
+    );
+
+    /**
+     * Invoked when an action occurs.
+     *
+     * @param e the event to be processed
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    /**
+     * This method gets called when a bound property is changed.
+     *
+     * @param evt A PropertyChangeEvent object describing the event source
+     *            and the property that has changed.
+     */
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
