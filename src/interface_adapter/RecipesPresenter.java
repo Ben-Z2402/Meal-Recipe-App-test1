2828d7718f05b5ea76b1f80f4bb7793544d22de9
Iris <irisss1717@gmail.com>
2023-11-27 18:27:47 -0500

    Revert "started recipes view"
    This reverts commit e7b656a044ac76ba027a6989ee2105858da7c255.

--- src/interface_adapter/RecipesPresenter.java
+++ src/interface_adapter/RecipesPresenter.java
@@ -4,22 +4,10 @@ import use_case.weekly_diet.WeeklyDietOutputBoundary;
 import use_case.weekly_diet.WeeklyDietInputData;
 import use_case.weekly_diet.WeeklyDietOutputData;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
 public class RecipesPresenter implements WeeklyDietOutputBoundary {
 
-    private final RecipesViewModel recipesViewModel;
-    private ViewManagerModel viewManagerModel;
-
-    public RecipesPresenter(ViewManagerModel viewManagerModel, RecipesViewModel recipesViewModel) {
-        this.viewManagerModel = viewManagerModel;
-        this.recipesViewModel = recipesViewModel;
-    }
-
     @Override
-    public void prepareSuccessView(WeeklyDietOutputData response) {
-        LocalDateTime responseTime = LocalDateTime.parse(response.getCreationTime());
-        response.setCreationTime(responseTime.format(DateTimeFormatter.ofPattern("hh:mm:ss")));
+    public void prepareSuccessView(WeeklyDietOutputData diet) {
+
     }
 }
