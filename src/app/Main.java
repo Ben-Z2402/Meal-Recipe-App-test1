159bc586c66af1d098adace70a14314753a9ca91
Ben Zheng <benz.zheng@mail.utoronto.ca>
2023-11-30 21:48:01 -0500

    modified main, signupview, and ViewManager

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,16 +1,30 @@
 package app;
+import interface_adapter.Login.LoginViewModel;
+import interface_adapter.Signup.SignupViewModel;
+import interface_adapter.ViewManagerModel;
+import view.LoginView;
+import view.SignupView;
+import view.ViewManager;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class Main {
-    JFrame application = new JFrame("test");
-    application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-    CardLayout cardLayout = new CardLayout();
+    public static void main(String[] args) {
 
-    JPanel views = new JPanel(cardLayout);
-    application.add(views);
+        JFrame application = new JFrame("test");
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-    ViewManagerModel viewManagerModel = new ViewManagerModel();
-    new ViewManage(views, cardLayout, viewManagerModel);
+        CardLayout cardLayout = new CardLayout();
+
+        JPanel views = new JPanel(cardLayout);
+        application.add(views);
+
+        ViewManagerModel viewManagerModel = new ViewManagerModel();
+        new ViewManager(views, cardLayout, viewManagerModel);
+
+        LoginViewModel loginViewModel = new LoginViewModel();
+        SignupViewModel signupViewModel = new SignupViewModel();
+    }
 }
