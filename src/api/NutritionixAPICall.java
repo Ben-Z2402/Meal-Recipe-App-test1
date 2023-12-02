a8f6395f6d46af00bcee42bb541293ad941cda81
Yakkuu <jack2016zhang@gmail.com>
2023-12-02 14:11:55 -0500

    updated NutritionixAPICall to take in more parameters

--- src/api/NutritionixAPICall.java
+++ src/api/NutritionixAPICall.java
@@ -1,10 +1,6 @@
 package api;
 
 import okhttp3.*;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONException;
-import com.alibaba.fastjson.JSONObject;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -13,16 +9,32 @@ import java.io.IOException;
 public class NutritionixAPICall {
     public static void main(String[] args) throws IOException {
         NutritionixAPICall apicall = new NutritionixAPICall();
-        System.out.println(apicall.caloriesBurned("swim for 1 day"));
+        String exercisePerformed = "swam 2 hour";
+        String gender = "male";
+        int weight = 75;
+        int height = 180;
+        int age = 30;
+        // gender, age and height does not appear to affect calculations
+        String query =
+                "{\n" +
+                        "\"query\" : \"" + exercisePerformed + "\",\n" +
+                        "\"gender\" : \"" + gender + "\",\n" +
+                        "\"weight_kg\" : \"" + weight + "\",\n" +
+                        "\"height_cm\" : \"" + height + "\",\n" +
+                        "\"age\" : \"" + age + "\"\n" +
+                        "}";
+        System.out.println(query);
+        System.out.println(apicall.caloriesBurned(query));
     }
 
-    public int caloriesBurned(String exercisePerformed) throws IOException {
+    public int caloriesBurned(String query) throws IOException {
         try{
             OkHttpClient client = new OkHttpClient();
 
 //            String exerciseDuration = exerciseName + " for " + String.valueOf(minutesPerformed) + " minutes";
             MediaType mediaType = MediaType.parse("application/json");
-            RequestBody body = RequestBody.create(mediaType, "{\n  \"query\": \"" + exercisePerformed + "\"\n}");
+            RequestBody body = RequestBody.create(mediaType, query);
+//            RequestBody body = RequestBody.create(mediaType, "{\n  \"query\": \"" + exercisePerformed + "\"\n}");
             Request request = new Request.Builder()
                     .url("https://trackapi.nutritionix.com/v2/natural/exercise")
                     .post(body)
@@ -33,8 +45,9 @@ public class NutritionixAPICall {
 
             Response response = client.newCall(request).execute();
 
-            // This is the staring representation of the response body (looks exactly like a JSON file).
+            // This is the string representation of the response body (looks exactly like a JSON file).
             String responseBody = response.body().string();
+//            System.out.println(responseBody);
             JSONObject JSONResponseBody = new JSONObject(responseBody);
             JSONArray exerciseInfo = JSONResponseBody.getJSONArray("exercises");
             return exerciseInfo.getJSONObject(0).getInt("nf_calories");
