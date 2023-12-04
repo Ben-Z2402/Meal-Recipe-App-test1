dbfd8b759a5de5880133bf06a2cadd89c48a1ffc
Gabriel Lee <gabriel.lee@mail.utoronto.ca>
2023-12-04 00:58:04 -0500

    made some changes to signupview

--- src/use_case/Signup/SignupInteractor.java
+++ src/use_case/Signup/SignupInteractor.java
@@ -37,9 +37,9 @@ public class SignupInteractor implements SignupInputBoundary {
         } else if (gender.equals("Male") && !signupInputData.getGender().equals("Female")) {
             userPresenter.prepareFailView("Input either \"Male\" or \"Female\".");
         } else if ((0.0 <= weight) && (weight <= 300.0)) {
-            userPresenter.prepareFailView("Enter a weight between 0.0kg and 300kg.");
+            userPresenter.prepareFailView("Enter a weight between 0.0kg and 300.0kg.");
         } else if ((0.0 <= height) && (height <= 250.0)) {
-            userPresenter.prepareFailView("Enter a height between 0.0cm and 250cm.");
+            userPresenter.prepareFailView("Enter a height between 0.0cm and 250.0cm.");
         } else if ((0 <= age) && (age <= 120)) {
             userPresenter.prepareFailView("Enter an age between 0 years and 120 years.");
         } else {
