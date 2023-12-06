759f88445a07a6a0a846c5c66c87dd40c60bc9a2
Gabriel Lee <gabriel.lee@mail.utoronto.ca>
2023-12-05 20:09:21 -0500

    Updated UserProfile to take recommendedDailyCalories

--- src/entity/CommonUserProfileFactory.java
+++ src/entity/CommonUserProfileFactory.java
@@ -6,7 +6,9 @@ public class CommonUserProfileFactory implements UserProfileFactory {
 
     @Override
     public UserProfile create(String username, String password, String gender, float weight, float height, int age,
-                              ArrayList<String> dietaryRestrictions, float weeklyBudget) {
-        return new UserProfile(username, password, gender, weight, height, age, dietaryRestrictions, weeklyBudget);
+                              ArrayList<String> dietaryRestrictions, float weeklyBudget,
+                              float recommendedDailyCalories) {
+        return new UserProfile(username, password, gender, weight, height, age, dietaryRestrictions, weeklyBudget,
+                recommendedDailyCalories);
     }
 }
